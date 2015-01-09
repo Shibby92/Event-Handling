5004e52520136af056b8d9c0bdb1a2ade7b9549c
Shibby92 <haris_000@hotmail.com>
2015-01-09 12:56:15 +0100

    Ispravljen suvisan static kod klasnih varijabli

--- src/Circle2.java
+++ src/Circle2.java
@@ -15,10 +15,10 @@ import javax.swing.Timer;
  *
  */
 public class Circle2 {
-	private static int x = 400;
-	private static int y = 300;
-	private static int frameX = 0;
-	private static int frameY = 0;
+	private int x = 400;
+	private int y = 300;
+	private int frameX = 0;
+	private int frameY = 0;
 
 	/**
 	 * Kreira frame na koji lijepi panel i testira ga
